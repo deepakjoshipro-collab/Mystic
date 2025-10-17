8981d04d7dfeca075f36ba214e706b6114012b86
deepakjoshipro-collab <deepakjoshipro@gmail.com>
2025-10-17 20:42:52 +0530

    Add files via upload

---
+++ kalash.js
@@ -0,0 +1,15 @@
+module.exports = {
+  port: "3000",
+  prefix: "$",
+  client: "Dusty",
+  client_id: "1411020013040697445",
+  client_secret: "0pb1BEM10UqqvMiuIg-SsIq625YKuPqM",
+  redirect_uri: "https://c8c1fed9-b49f-4ee2-8360-971444653626-00-18i6kbfem165v.janeway.replit.dev/",
+  footer: "",
+  support: "https://discord.com",
+  wehbook: "https://discord.com/api/webhooks/1428426802925408389/O9bZiYE1mkT1hqsHQT65e1rP6xnfm3txXWopPTqYJzsx7A7rTi8jp7jE-1n1W_XAnflp", 
+  owners: ["925424598680481873","827146821968330802"],
+  authLink: "https://discord.com/oauth2/authorize?client_id=1411020013040697445&response_type=code&redirect_uri=https%3A%2F%2Fc8c1fed9-b49f-4ee2-8360-971444653626-00-18i6kbfem165v.janeway.replit.dev%2F&scope=identify+guilds.join+guilds",
+
+  bn:["https://media.discordapp.net/attachments/1158752755461128262/1206837119352307772/20240213_110913_0000.png?ex=65dd75a7&is=65cb00a7&hm=de18377388e2b42b11b324875cef37ec2cdac75e6b2c0bea574631c482e47a39&"],
+}
